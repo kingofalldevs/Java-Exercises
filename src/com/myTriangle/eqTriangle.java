c5e556da2ce0a538769d2cdd8500a9532acbadee
kingofalldevs <johnick777@yahoo.com>
2026-09-08 04:59:32 +0000

    added some meaning

--- src/com/myTriangle/eqTriangle.java
+++ src/com/myTriangle/eqTriangle.java
@@ -7,7 +7,8 @@ public class eqTriangle {
     }
 
     public static void triangle() {
-        for(int i= 1, j = 4; i < 10 && j < 5; j--, i+= 2){
+        int rows = 5;
+        for(int i= 1, j = 4; i < 10 && j < rows; j--, i+= 2){
             System.out.println("\t\t\t"+" ".repeat(j) + "*".repeat(i));
         }
         System.out.println();
