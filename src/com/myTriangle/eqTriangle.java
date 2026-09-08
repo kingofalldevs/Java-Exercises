3c4913078f1b139a6fa18ca05f5c6ac11a660360
kingofalldevs <johnick777@yahoo.com>
2026-09-08 05:06:04 +0000

    added some comments to the code

--- src/com/myTriangle/eqTriangle.java
+++ src/com/myTriangle/eqTriangle.java
@@ -10,6 +10,9 @@ public class eqTriangle {
 
     public static void triangle() {
         int rows = 5;
+        // i declared rows = 5 because i need 5 rows and in that 5 rows i needed
+        // a perfect triangle formed from it \t's adds some space which pushes
+        // the whole triangle to the right and the repeat func is used to repeat items as shown in the for loop below
         for(int i= 1, j = 4; i < 10 && j < rows; j--, i+= 2){
             System.out.println("\t\t\t"+" ".repeat(j) + "*".repeat(i));
         }
