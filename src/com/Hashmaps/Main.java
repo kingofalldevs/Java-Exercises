9f6a8d1006170e7910af32748d93e8ef94f64451
kingofalldevs <johnick777@yahoo.com>
2026-09-19 09:05:51 +0000

    Intro to hashmaps

---
+++ src/com/Hashmaps/Main.java
@@ -0,0 +1,18 @@
+package com.Hashmaps;
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(1, "Diana");
+        map.put(2,"Beauty");
+        map.put(3,"Sammy");
+        map.put(4,"Baron");
+        map.put(5,"Joseph");
+
+
+
+
+    }
+}
