ec9ec2101b55379cc2a965ae8eb001ffdf6a7bfe
kingofalldevs <johnick777@yahoo.com>
2026-09-20 08:42:19 +0000

    Writing the code for a logistics delivery system

---
+++ src/com/LogisticDeliverySystem/DeliveryPackage.java
@@ -0,0 +1,28 @@
+package com.LogisticDeliverySystem;
+
+public class DeliveryPackage {
+    private String trackingId;
+    private String destination;
+    private double weight;
+    private String status;
+    //constructor
+    DeliveryPackage(String trackingId,String destination, double weight, String status){
+        this.trackingId = trackingId;
+        this.destination = destination;
+        this.weight = weight;
+        this.status = status;
+    }
+    //setters
+    public String getTrackingId() { return trackingId;}
+    public String getDestination() {return destination;}
+    public double getWeight() {return weight;}
+    public String getStatus() {return status;}
+    //getters
+    public void setTrackingId(String trackingId) {this.trackingId = trackingId;}
+    public void setDestination(String destination) {this.destination = destination;}
+    public void setWeight(double weight) { this.weight = weight;}
+    public void setStatus(String status) {this.status = status;}
+}
+//Attributes to create: trackingId (String), destination (String), weight (double), and status (String).
+// Rules: Make all fields completely private. Provide a constructor to initialize them and create the appropriate public getters and setters.
+// Method to design: A method called calculateShippingCost() that returns a double calculated by multiplying the weight by a base rate (e.g., 20 GHS per kg).
