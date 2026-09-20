8bf6317d3e62a942e17110a13f5b1d1de4f32658
kingofalldevs <johnick777@yahoo.com>
2026-09-20 09:26:27 +0000

    Experimented with Super() keyword

--- src/com/LogisticDeliverySystem/ExpressPackage.java
+++ src/com/LogisticDeliverySystem/ExpressPackage.java
@@ -3,4 +3,14 @@ package com.LogisticDeliverySystem;
 public class ExpressPackage extends DeliveryPackage{
     private boolean isOvernight;
 
+    ExpressPackage(String trackingId,String destination, double weight,String status,boolean isOvernight){
+        super(trackingId, destination, weight, status);
+        this.isOvernight= isOvernight;
+    }
+
+    if(isOvernight == true;){
+
+    }
+
+
 }
