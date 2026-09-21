360f6e4a59a7eddbd25f7355ff192dad87d8c944
kingofalldevs <johnick777@yahoo.com>
2026-09-21 12:34:40 +0000

    Understand the concept of abstraction. Implement 3 classes and make the parent class(Vehicle) of the Car class abstract using the abstract keyword

---
+++ src/Abstraction/Car.java
@@ -0,0 +1,8 @@
+package Abstraction;
+
+public class Car extends Vehicle{
+
+    public void go(){
+        System.out.println("The car is moving");
+    }
+}
---
+++ src/Abstraction/Main.java
@@ -0,0 +1,12 @@
+package Abstraction;
+
+public class Main {
+    public static void main(String[] args) {
+        //Abstraction = Abstract classes cannot be instantiated, but they can have a subclass
+        //              Abstract methods are declared without an implementation.
+    }
+
+    Car car = new Car();
+
+
+}
