c5ed39b2a8ed65d652fda5c66a925d1c2719be2d
kingofalldevs <johnick777@yahoo.com>
2026-09-23 05:32:31 +0000

    Implement hardcoded data for the vehicle fields

--- src/DeliveryManagementSystem/Operations.java
+++ src/DeliveryManagementSystem/Operations.java
@@ -24,9 +24,31 @@ public class Operations {
     }
     //add vehicles
     public void addVehicles(){
+        //motorCycle 1
         motorCycle.setVehicleId("EI-44urj");
-        motorCycle.setBrand("Toyota");
+        motorCycle.setBrand("Suzuki");
         motorCycle.setCapacity(458.34);
         motorCycles.addFirst(motorCycle);
+        //motorcycle 2
+        motorCycle.setVehicleId("MI-44urj");
+        motorCycle.setBrand("Yamaha");
+        motorCycle.setCapacity(831.34);
+        motorCycles.addFirst(motorCycle);
+        //car 1
+        car.setBrand("Ford");
+        car.setCapacity(648.21);
+        car.setVehicleId("QW-2335");
+        //car 2
+        car.setBrand("Nissan");
+        car.setCapacity(648.21);
+        car.setVehicleId("II-78165");
+        //Truck 1
+        truck.setBrand("Toyota");
+        truck.setCapacity(770.43);
+        truck.setVehicleId("YU-57438");
+        //Truck 2
+        truck.setBrand("Man");
+        truck.setCapacity(770.43);
+        truck.setVehicleId("KU-5738");
     }
 }
