ad84424e0c9d6f3f2471c7dcbf6b693942c8caf4
kingofalldevs <johnick777@yahoo.com>
2026-09-23 10:41:16 +0000

    create object instance 2 for the second motorcycle we are adding and write a for loop in operations in a method to list all motorcycles and then call the methid in the main class

--- src/DeliveryManagementSystem/Main.java
+++ src/DeliveryManagementSystem/Main.java
@@ -6,25 +6,32 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         DeliveryOrder deliveryOrder = new DeliveryOrder();
+        Operations operations = new Operations();
 
-        boolean userSession = true;
-        do {
-            System.out.println("Welcome to the Delivery Store Store");
-            System.out.println("1.Register a customer \n2.Find order by ID \n3.Place Order \n4.Display all orders \n5.Exit Application");
-            int userInput = input.nextInt();
-            if(userInput ==5 ){
-                userSession =false;
-                System.out.println("Exiting Application ....");
-            } else if (userInput == 1) {
-                System.out.println("Register a customer");
-            }else if(userInput == 2){
-                System.out.println("Find order \n Enter Order ID: ");
+//        boolean userSession = true;
+//        do {
+//            System.out.println("Welcome to the Delivery Store");
+//            System.out.println("1.Register a customer \n2.Find order by ID \n3.Place Order \n4.Display all vehicles \n5.Exit Application");
+//            int userInput = input.nextInt();
+//            if(userInput ==5 ){
+//                userSession =false;
+//                System.out.println("Exiting Application ....");
+//            } else if (userInput == 1) {
+//                System.out.println("Register a customer");
+//            }else if(userInput == 2){
+//                System.out.println("Find order \n Enter Order ID: ");
+//
+//            }else if (userInput == 3){
+//                System.out.println("Place Order");
+//            }else if(userInput == 4){
+//                System.out.println("Display all Vehicles");
+//                operations.findVehicle();
+//            }
+//        }while(userSession);
 
-            }else if (userInput == 3){
-                System.out.println("Place Order");
-            }else if(userInput == 4){
-                System.out.println("Display all orders");
-            }
-        }while(userSession);
+        operations.addVehicles();
+        operations.listMotorCycles();
     }
+
+
 }
--- src/DeliveryManagementSystem/Operations.java
+++ src/DeliveryManagementSystem/Operations.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class Operations {
     //vehicles
     MotorCycle motorCycle = new MotorCycle();
+    MotorCycle secondMotorCycle = new MotorCycle();
     Car car = new Car();
     Truck truck = new Truck();
     //Delivery Types
@@ -29,26 +30,38 @@ public class Operations {
         motorCycle.setBrand("Suzuki");
         motorCycle.setCapacity(458.34);
         motorCycles.addFirst(motorCycle);
-        //motorcycle 2
-        motorCycle.setVehicleId("MI-44urj");
-        motorCycle.setBrand("Yamaha");
-        motorCycle.setCapacity(831.34);
-        motorCycles.addFirst(motorCycle);
+        //motorcycle 2(Create another object for this )
+        secondMotorCycle.setVehicleId("MI-44urj");
+        secondMotorCycle.setBrand("Yamaha");
+        secondMotorCycle.setCapacity(831.34);
+        motorCycles.add(secondMotorCycle);
         //car 1
         car.setBrand("Ford");
         car.setCapacity(648.21);
         car.setVehicleId("QW-2335");
+        cars.addFirst(car);
         //car 2
         car.setBrand("Nissan");
         car.setCapacity(648.21);
         car.setVehicleId("II-78165");
+        cars.add(car);
         //Truck 1
         truck.setBrand("Toyota");
         truck.setCapacity(770.43);
         truck.setVehicleId("YU-57438");
+        trucks.addFirst(truck);
         //Truck 2
         truck.setBrand("Man");
         truck.setCapacity(770.43);
         truck.setVehicleId("KU-5738");
+        trucks.add(truck);
+    }
+
+    Vehicle[] vehicle = {car, truck, motorCycle};
+
+    public void listMotorCycles(){
+        for (MotorCycle cycle : motorCycles) {
+            System.out.println(cycle.getBrand() + " " + cycle.getVehicleId() + " " + cycle.getCapacity());
+        }
     }
 }
